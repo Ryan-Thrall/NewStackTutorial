e51b3fd94b76312711ee76168b63710d8cc26f4d
Ryan-Thrall <ryanthrall04@gmail.com>
2023-09-02 11:43:09 -0600

    Get and Post Comments Backend Working

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -14,6 +14,10 @@ module.exports = (sequelize, DataTypes) => {
     },
 
   });
-
+  Posts.associate = (models) => {
+    Posts.hasMany(models.Comments, {
+      onDelete: "cascade",
+    })
+  }
   return Posts;
 };
